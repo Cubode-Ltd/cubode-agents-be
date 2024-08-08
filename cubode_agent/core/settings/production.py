2c47805430a4b916a8474eee02c2c755c0a6d9c3
cubode <david.garcia@cubode.com>
2024-08-08 14:18:30 +0200

    Infrastructure as code implementation

--- cubode_agent/core/settings/production.py
+++ cubode_agent/core/settings/production.py
@@ -1,1 +1,57 @@
+import os
+from core.settings.base import *
+from core.settings.base import BASE_DIR
+
 DEBUG = False
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [
+            BASE_DIR / 'assets' / 'html',
+            BASE_DIR / 'assets' / 'templates',
+        ],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
+
+# AWS S3 configurations
+AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID", "")
+AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY", "")
+AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME", "")
+
+AWS_S3_REGION_NAME = 'us-east-1'
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+AWS_S3_ENDPOINT_URL = os.environ.get("AWS_S3_ENDPOINT_URL", "")
+AWS_S3_SIGNATURE_VERSION = "s3v4"
+AWS_S3_FILE_OVERWRITE = False
+
+AWS_LOCATION = os.environ.get("AWS_LOCATION", "static")
+AWS_QUERYSTRING_AUTH = False
+AWS_DEFAULT_ACL = "public-read"
+AWS_QUERYSTRING_EXPIRE = 300
+
+# CLOUD FRONT
+AWS_S3_CLOUDFRONT_DOMAIN = os.environ.get("AWS_S3_CLOUDDOMAIN", "")
+AWS_S3_CUSTOM_DOMAIN = AWS_S3_CLOUDFRONT_DOMAIN + AWS_STORAGE_BUCKET_NAME
+AWS_CLOUDFRONT_KEY_ID = os.environ.get("AWS_CLOUDFRONT_KEY_ID","").strip()
+AWS_CLOUDFRONT_KEY = os.environ.get("AWS_CLOUDFRONT_KEY", "").replace("\\n", "\n").encode('ascii').strip()
+
+# Static files (CSS, JavaScript, Images)
+STATIC_LOCATION = "static"
+STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+# Media files
+PUBLIC_MEDIA_LOCATION = "media"
+MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
--- infra/main.tf
+++ infra/main.tf
@@ -2,10 +2,134 @@ provider "aws" {
   region = "us-east-1"
 }
 
-locals {
-  public_key_path = "${path.module}/keys/${var.instance_name}-key.pub"
+# --- 0. Steps 
+# 1. Create the S3 Bucket and Permissions
+# 2. Create the Policy, Group and User for S3 / EC2
+# 3. Create the EC2 with Security Group 80, 443 and 22
+# 4. Create the CloudFront and link to S3.
+
+
+
+# --- 1. Create S3 Bucket
+resource "aws_s3_bucket" "graph_cubode_static_bucket" {
+  bucket = "${var.instance_name}-static-bucket"
+  tags = {
+    Name        = "${var.instance_name}-static-bucket"
+    Environment = "production"
+  }
 }
 
+# Use aws_s3_bucket_acl to set the ACL
+resource "aws_s3_bucket_acl" "graph_cubode_static_bucket_acl" {
+  bucket = aws_s3_bucket.graph_cubode_static_bucket.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_versioning" "graph_cubode_static_bucket_versioning" {
+  bucket = aws_s3_bucket.graph_cubode_static_bucket.bucket
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_cors_configuration" "graph_cubode_static_bucket_cors" {
+  bucket = aws_s3_bucket.graph_cubode_static_bucket.bucket
+
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["GET"]
+    allowed_origins = ["*"]
+    max_age_seconds = 3000
+  }
+}
+
+
+# --- 2.- Policy, Group and User
+resource "aws_iam_policy" "s3_ec2_access_policy" {
+  name        = "s3-ec2-access-policy"
+  description = "Policy granting access to S3 and EC2"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "s3:ListBucket",
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:DeleteObject"
+        ],
+        Resource = [
+          "arn:aws:s3:::${var.instance_name}-static-bucket",
+          "arn:aws:s3:::${var.instance_name}-static-bucket/*"
+        ]
+      },
+      {
+        Effect = "Allow",
+        Action = [
+          "ec2:DescribeInstances",
+          "ec2:StartInstances",
+          "ec2:StopInstances"
+        ],
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+# Create an IAM group and attach the policy
+resource "aws_iam_group" "s3_ec2_access_group" {
+  name = "s3-ec2-access-group"
+}
+
+resource "aws_iam_group_policy_attachment" "s3_ec2_policy_attachment" {
+  group      = aws_iam_group.s3_ec2_access_group.name
+  policy_arn = aws_iam_policy.s3_ec2_access_policy.arn
+}
+
+# Create an IAM user and attach the user to the group
+resource "aws_iam_user" "s3_ec2_user" {
+  name = "s3-ec2-user"
+}
+
+resource "aws_iam_user_group_membership" "s3_ec2_user_membership" {
+  user = aws_iam_user.s3_ec2_user.name
+  groups = [aws_iam_group.s3_ec2_access_group.name]
+}
+
+# Create an IAM role that EC2 instances can assume
+resource "aws_iam_role" "graph_cubode_s3_access_role" {
+  name = "${var.instance_name}_s3_access_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        },
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+# Attach the S3 and EC2 access policy to the IAM role
+resource "aws_iam_role_policy_attachment" "s3_ec2_role_policy_attachment" {
+  role       = aws_iam_role.graph_cubode_s3_access_role.name
+  policy_arn = aws_iam_policy.s3_ec2_access_policy.arn
+}
+
+# Create an instance profile to link the IAM role to the EC2 instance
+resource "aws_iam_instance_profile" "graph_cubode_s3_access_profile" {
+  name = "${var.instance_name}_s3_access_profile"
+  role = aws_iam_role.graph_cubode_s3_access_role.name
+}
+
+# --- 3.- EC2
 resource "aws_key_pair" "graph_cubode_key" {
   key_name   = "${var.instance_name}-key"
   public_key = file("${path.module}/keys/${var.instance_name}-key.pub")
@@ -20,6 +144,8 @@ resource "aws_instance" "graph-cubode" {
 
   # Security group with SSH access
   vpc_security_group_ids = [aws_security_group.allow_ssh_and_web.id]
+
+  iam_instance_profile = aws_iam_instance_profile.graph_cubode_s3_access_profile.name
 
   # User data script for installing Docker, Git, and configuring the environment
   user_data = <<-EOF
@@ -86,3 +212,78 @@ resource "aws_security_group" "allow_ssh_and_web" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+# --- 4. - CloudFront and S3 Policy change
+resource "aws_cloudfront_origin_access_identity" "graph_cubode_oai" {
+  comment = "${var.instance_name} OAI for S3 bucket"
+}
+
+resource "aws_cloudfront_distribution" "graph_cubode_distribution" {
+  origin {
+    domain_name = aws_s3_bucket.graph_cubode_static_bucket.bucket_regional_domain_name
+    origin_id   = "${var.instance_name}-s3-origin"
+
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.graph_cubode_oai.cloudfront_access_identity_path
+    }
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  comment             = "CloudFront distribution for ${var.instance_name} static content"
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "${var.instance_name}-s3-origin"
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 86400
+    max_ttl                = 31536000
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  tags = {
+    Name = "${var.instance_name}-cloudfront-distribution"
+  }
+}
+
+# Update S3 Bucket Policy to Allow CloudFront OAI Access
+resource "aws_s3_bucket_policy" "graph_cubode_bucket_policy" {
+  bucket = aws_s3_bucket.graph_cubode_static_bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          AWS = aws_cloudfront_origin_access_identity.graph_cubode_oai.iam_arn
+        },
+        Action   = "s3:GetObject",
+        Resource = "${aws_s3_bucket.graph_cubode_static_bucket.arn}/*"
+      }
+    ]
+  })
+}
+
+# --- 5. Outputs
+
