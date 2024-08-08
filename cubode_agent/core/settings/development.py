a00bf5f9d4f1838c9484fe8561ce8ba103dd8ce9
cubode <david.garcia@cubode.com>
2024-08-08 16:03:33 +0200

    bug fixing

--- cubode_agent/core/settings/development.py
+++ cubode_agent/core/settings/development.py
@@ -6,6 +6,24 @@ from core.settings.base import INSTALLED_APPS, MIDDLEWARE, BASE_DIR
 DEVELOPMENT = True
 DEBUG = True
 
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [
+            BASE_DIR / 'assets' / 'html',
+            BASE_DIR / 'assets' / 'templates',
+        ],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
